2ea5b209dffa69db03f5ca1927be1f1b00d07f77
Raul Neto <rjbnetto@gmail.com>
2023-10-18 19:44:17 -0300

    Atividade Concluída

--- Atividades/TratamentoExcecao/ExemploTratamentoExcecao.java
+++ Atividades/TratamentoExcecao/ExemploTratamentoExcecao.java
@@ -1,24 +1,27 @@
 import java.util.Scanner;
-
 public class ExemploTratamentoExcecao {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int soma = 0;
 
         try {
-            System.out.print("Digite um número: ");
-            int numero = Integer.parseInt(scanner.nextLine());
+            while (true) {
+                System.out.print("Digite um numero inteiro positivo (ou um numero negativo para encerrar): ");
+                int numero = scanner.nextInt();
+                
+                if (numero < 0) {
+                    System.out.println("Numeros negativos nao sao permitidos. Encerrando o programa.");
+                    break;
+                }
 
-            int resultado = 10 / numero;
-
-            System.out.println("O resultado da divisão de 10 por " + numero + " é: " + resultado);
-        } catch (ArithmeticException e) {
-            System.err.println("Erro: Divisão por zero. Por favor, insira um número diferente de zero.");
-        } catch (NumberFormatException e) {
-            System.err.println("Erro: O valor inserido não é um número inteiro válido.");
+                soma += numero;
+            }
         } catch (Exception e) {
-            System.err.println("Ocorreu um erro inesperado: " + e.getMessage());
+            System.out.println("Ocorreu um erro. Certifique-se de digitar um numero inteiro valido.");
         } finally {
-            System.out.println("O programa foi concluído.");
+            scanner.close();
         }
+
+        System.out.println("A soma dos numeros positivos e: " + soma);
     }
 }
